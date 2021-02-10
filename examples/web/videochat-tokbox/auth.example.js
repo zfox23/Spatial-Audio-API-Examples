4103e912de28f134a234a7384877ce24cc139fd6
Zach Fox <fox@highfidelity.io>
2021-02-10 01:53:36 -0500

    Beginnings of a TokBox + Spatial Audio Example App

---
+++ examples/web/videochat-tokbox/auth.example.js
@@ -0,0 +1,10 @@
+// You can obtain a test JWT (for testing purposes only) from the High Fidelity Spatial Audio API Developer Console:
+// https://account.highfidelity.com/dev/account
+const HIFI_AUDIO_JWT = "";
+
+// You can obtain static authorization information for testing purposes only from within your Project's settings page.
+// You can access that page via your TokBox Account Page:
+// https://tokbox.com/account/#/
+const TOKBOX_API_KEY = "";
+const TOKBOX_SESSION_ID = "";
+const TOKBOX_TOKEN = "";
