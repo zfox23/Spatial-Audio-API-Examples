79574b2a7b4719929aac6d46f795aa5042a71b5c
Zach <fox@highfidelity.com>
2021-03-29 15:37:49 -0400

    Some changes

--- experiments/react-native/simple/App.tsx
+++ experiments/react-native/simple/App.tsx
@@ -13,7 +13,6 @@ import {
 } from 'react-native-webrtc';
 
 import {
-    getBestAudioConstraints,
     HiFiCommunicator
 } from 'hifi-spatial-audio';
 
@@ -29,47 +28,52 @@ class ConnectDisconnectButton extends React.Component<{}, { isConnected: boolean
         };
     }
 
-    async onConnectDisconnectButtonPressed() {
+    onConnectDisconnectButtonPressed() {
         // Disable the Connect button after the user clicks it so we don't double-connect.
         this.setState({ disabled: true });
         this.setState({ buttonText: "Connecting..." });
 
-        // Get the audio media stream associated with the user's default audio input device.
-        let audioMediaStream;
-        try {
-            audioMediaStream = await mediaDevices.getUserMedia({ audio: getBestAudioConstraints(), video: false });
-        } catch (e) {
-            return;
-        }
-
-        let hifiCommunicator = new HiFiCommunicator();
-        await hifiCommunicator.setInputAudioMediaStream(audioMediaStream);
-
         let hiFiAudioJWT = "test";
-        try {
-            await hifiCommunicator.connectToHiFiAudioAPIServer(hiFiAudioJWT);
-        } catch (e) {
-            console.error(`Error connecting to High Fidelity:\n${e}`);
-            this.setState({ disabled: false });
-            this.setState({ buttonText: `Connection error. Retry?` });
-            return;
-        }
 
-        // Show the user that we're connected by changing the text on the button.
-        this.setState({ disabled: false });
-        this.setState({ buttonText: `Connected!` });
+        console.log(`Creating HiFiCommunicator...`);
+        let hifiCommunicator = new HiFiCommunicator();
 
-        // // Set the `srcObject` on our `audio` DOM element to the final, mixed audio stream from the High Fidelity Audio API Server.
-        // document.querySelector(`.outputAudioEl`).srcObject = hifiCommunicator.getOutputAudioMediaStream();
-        // // We explicitly call `play()` here because certain browsers won't play the newly-set stream automatically.
-        // document.querySelector(`.outputAudioEl`).play();
+        // Get the audio media stream associated with the user's default audio input device.
+        console.log(`Getting audio input stream...`);
+        mediaDevices.getUserMedia({ audio: true, video: false })
+            .then((inputMediaStream) => {
+                console.log(`Setting audio input stream on HiFiCommunicator...`);
+                hifiCommunicator.setInputAudioMediaStream(inputMediaStream);
+
+                console.log(`Connecting to HiFi Audio API Server...`);
+                hifiCommunicator.connectToHiFiAudioAPIServer(hiFiAudioJWT)
+                    .then((response) => {
+                        console.log(`Connected!`);
+                        // Show the user that we're connected by changing the text on the button.
+                        this.setState({ disabled: false });
+                        this.setState({ buttonText: `Connected!` });
+                
+                        // // Set the `srcObject` on our `audio` DOM element to the final, mixed audio stream from the High Fidelity Audio API Server.
+                        // document.querySelector(`.outputAudioEl`).srcObject = hifiCommunicator.getOutputAudioMediaStream();
+                        // // We explicitly call `play()` here because certain browsers won't play the newly-set stream automatically.
+                        // document.querySelector(`.outputAudioEl`).play();
+                    })
+                    .catch((e) => {
+                        console.error(`Error connecting to High Fidelity:\n${e}`);
+                        this.setState({ disabled: false });
+                        this.setState({ buttonText: `Connection error. Retry?` });
+                    })
+            })
+            .catch((e) => {
+                console.error(`Error getting \`audioMediaStream\`:\n${e}`);
+            })
     }
 
     render() {
         return (
             <Button
                 title={this.state.buttonText}
-                onPress={this.onConnectDisconnectButtonPressed}
+                onPress={this.onConnectDisconnectButtonPressed.bind(this)}
                 disabled={this.state.disabled}
             />
         );
